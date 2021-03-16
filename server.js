80eb1d45912c2809596db2995e486cc5e76594ce
tylermcarthur <tylermcarthur@yahoo.com>
2021-03-16 15:10:47 -0500

    new commit

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.post('/api/public', async (req,res)=>{
 app.get('/api/public/opening_page', async(req,res)=>{
     try {
         const scenarios = pool.query("SELECT scenario_name FROM scenarios", (err, data) => {
+            res.end('boop')
             let arr1 = data.rows
             let arr2 = []
             arr1.forEach(element => {
