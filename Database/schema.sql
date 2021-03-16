11329aeea419e719fabf5e50a38343b233c663f8
tylermcarthur <tylermcarthur@yahoo.com>
2021-03-16 11:44:07 -0500

    2

--- Database/schema.sql
+++ Database/schema.sql
@@ -1,14 +1,12 @@
 
-CREATE DATABASE scenario_db;
-
 CREATE TABLE scenarios(
    scenario_ID SERIAL PRIMARY KEY NOT NULL,
    scenario_name TEXT NOT NULL,
-   opening_text TEXT NOT NULL,
+   opening_text TEXT NOT NULL
 );
 CREATE TABLE pages(
    page_ID SERIAL PRIMARY KEY NOT NULL,
    page_name TEXT NOT NULL,
    page_text TEXT NOT NULL,
-   scenario_ID INT FOREIGN KEY REFERENCES scenarios(scenario_ID)NOT NULL
+   scenario_ID int FOREIGN KEY REFERENCES scenarios(scenario_ID)
 );
