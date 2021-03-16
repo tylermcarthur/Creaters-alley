6ea5bb1f6c766198648dc07d3279ad237ab65720
tylermcarthur <tylermcarthur@yahoo.com>
2021-03-16 15:02:40 -0500

    new commit

--- public/app.js
+++ public/app.js
@@ -1,8 +1,8 @@
 const inputBox = document.querySelector('#inputBox');
 const iframe = document.querySelector('#iframe')
 
-inputBox.addEventListener('keydown', (e)=>{
-    if(e.code == 'Enter'){
+inputBox.addEventListener('keypress', (e)=>{
+    if(e.key == 'Enter'){
         iframe.setAttribute('src',`https://creaters-alley.herokuapp.com/api/public/${inputBox.value}`)
     }
 })
