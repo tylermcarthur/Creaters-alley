a9cc4b1e4c362ce574da5c318b3a170b95d195b7
tylermcarthur <tylermcarthur@yahoo.com>
2021-03-16 14:09:56 -0500

    new commit

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.post('/api/public', async (req,res)=>{
 app.get('/api/public/opening_page', async(req,res)=>{
     try {
         const client = await pool.connect
-        const students = pool.query("SELECT * FROM scenarios WHERE scenario_ID = 1", (err, data) => {
+        const students = pool.query("SELECT opening_text FROM scenarios WHERE scenario_ID = 1", (err, data) => {
             res.json(data.rows[0]);
             client.release();
         })
