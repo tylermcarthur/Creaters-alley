f6d9d8256a4338b8af08442f2b7b09f4a1fb44cb
tylermcarthur <tylermcarthur@yahoo.com>
2021-03-16 16:03:29 -0500

    new commit

--- server.js
+++ server.js
@@ -36,7 +36,6 @@ app.get('/api/public/opening_page', async(req,res)=>{
 // get specific
 app.get('/api/public/:id', async(req,res)=>{
     try {
-        res.end('boop')
         const name = req.params.id
         const x = pool.query(`SELECT opening_text FROM scenarios WHERE scenario_name = ${name}`,(err,data)=>{
             res.json(data.rows)
