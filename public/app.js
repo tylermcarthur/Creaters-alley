2af56f73198855bc7a179727ec7a7fba0e6d0569
tylermcarthur <tylermcarthur@yahoo.com>
2021-03-16 17:26:18 -0500

    new commit

--- public/app.js
+++ public/app.js
@@ -1,8 +1,15 @@
 const inputBox = document.querySelector('#inputBox');
 const iframe = document.querySelector('#iframe')
 
+// for selecting a scenario
 inputBox.addEventListener('keypress', (e)=>{
-    if(e.key == 'Enter'){
+    if(e.key == 'Enter' && inputBox.value.toLowerCase != 'play' && inputBox.value.toLowerCase != 'back' && inputBox.value.toLowerCase != 'delete'){
         iframe.setAttribute('src',`https://creaters-alley.herokuapp.com/api/public/${inputBox.value}`)
+    }
+})
+// for selecting Back
+inputBox.addEventListener('keypress', (e)=>{
+    if(e.key == 'Enter' && inputBox.value.toLowerCase == 'back'){
+        iframe.setAttribute('src',`https://creaters-alley.herokuapp.com/api/public/`)
     }
 })
