115f3910c46912facdf2225574b1022064eac9ad
tylermcarthur <tylermcarthur@yahoo.com>
2021-03-16 19:39:41 -0500

    new commit

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.get('/api/page/:id/:num',async(req,res)=>{
         const scenario_name = req.params.id
         const page_number = req.params.num
         pool.query(`SELECT * FROM pages WHERE pages.page_number = ${page_number} AND pages.scenario_name = '${scenario_name}'`,(err,data)=>{
-            res.json(`SELECT * FROM pages WHERE pages.page_number = ${page_number} AND pages.scenario_name = '${scenario_name}'`)
+            res.json(`SELECT * FROM pages WHERE pages.page_number = ${page_number} AND pages.scenario_name = '"${scenario_name}"'`)
         })
     } catch (err) {
         console.error(err.message)
