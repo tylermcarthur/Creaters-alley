0df4b2c86a0c0d01d8a8b218ec4667d2c25a3bbb
tylermcarthur <tylermcarthur@yahoo.com>
2021-03-17 14:19:51 -0500

    new commit

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.get('/api/page/:id/:num',async(req,res)=>{
 // this responds with all actions for that scenario name
 app.get('/api/actions/:name/:num',async(req,res)=>{
     try {
-        const page_number = req.params.id
+        const page_number = req.params.num
         const scenario_name = req.params.name
         pool.query(`SELECT * FROM actions WHERE actions.scenario_name = '${scenario_name}' AND actions.page_number = ${page_number}`,(err,data)=>{
             res.json(data)
