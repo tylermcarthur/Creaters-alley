776a833cd2d88f6fcb8a9f8eec918a54dfd1ad48
tylermcarthur <tylermcarthur@yahoo.com>
2021-03-17 11:30:08 -0500

    new commit

--- Database/schema.sql
+++ Database/schema.sql
@@ -1,14 +1,33 @@
+DROP TABLE IF EXISTS scenarios
+DROP TABLE IF EXISTS pages
+DROP TABLE IF EXISTS actions
+
 
 CREATE TABLE scenarios(
-   scenario_ID SERIAL PRIMARY KEY NOT NULL,
-   scenario_name TEXT NOT NULL,
-   opening_text TEXT NOT NULL
+   scenario_ID SERIAL,
+   scenario_name VARCHAR(255) NOT NULL UNIQUE,
+   opening_text TEXT,
+   PRIMARY KEY(scenario_ID)
 );
 CREATE TABLE pages(
-   page_ID SERIAL PRIMARY KEY NOT NULL,
-   page_number int NOT NULL,
-   page_text TEXT NOT NULL,
-   scenario_name TEXT
+   page_ID SERIAL,
+   scenario_name VARCHAR(255),
+   page_number INT,
+   page_text TEXT,
+   PRIMARY KEY(page_ID),
+   CONSTRAINT fk_scenarios
+      FOREIGN KEY(scenario_name)
+         REFERENCES scenarios(scenario_name)
+);
+CREATE TABLE actions(
+   actions_ID SERIAL,
+   page_ID INT,
+   actions_text TEXT,
+   page_number INT,
+   PRIMARY KEY(actions_ID),
+   CONSTRAINT fk_pages
+      FOREIGN KEY(page_ID)
+         REFERENCES pages(page_ID)
 );
 git add .
 git commit -m 'new commit'
